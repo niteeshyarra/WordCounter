157c66fc035c6c3e418df32df10a25ec5ebfa0d8
nyarra <niteeshyarra@quickenloans.com>
2020-10-23 15:05:10 -0400

    Script to count words

---
+++ wordCounter.py
@@ -0,0 +1,39 @@
+import os
+import socket
+
+directory = os.path.dirname(os.path.realpath(__file__))
+dataDirectory = directory+r"/home/data"
+totalWords = 0
+maxwords = 0
+fileWithMaxWords = ""
+
+for filename in os.listdir(dataDirectory):
+    filepath = os.path.join(dataDirectory, filename)
+    if filename.endswith(".txt"):
+        file = open(filepath)
+        data = file.read()
+        words = data.split()
+        wordCount = len(words)
+        if wordCount >= maxwords:
+            maxwords = wordCount
+            fileWithMaxWords = filename
+        totalWords += wordCount
+        file.close()
+
+hostname = socket.gethostname()
+ip_address = socket.gethostbyname(hostname)
+
+
+line1 = f"Hostname: {hostname}"
+line2 = f"IP Address: {ip_address}"
+line3 = "Total Words: "+str(totalWords)
+line4 = "Highest Words: "+str(maxwords)
+line5 = "File with highest words: "+fileWithMaxWords
+
+
+outputDirectoryPath = os.path.join(directory+r"\home", 'output')
+if not os.path.isdir(outputDirectoryPath):
+    os.mkdir(outputDirectoryPath)
+outputfilePath = os.path.join(outputDirectoryPath, 'output.txt')
+f = open(outputfilePath, 'w')
+f.writelines([line1, line2+"\n", line3+"\n", line4+"\n", line5+"\n"])
